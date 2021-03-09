7f4ea1930cff36de13020e59d5cbef2de9748ba9
janou <MhdKarim.Janou@infineon.com>
2021-03-09 19:19:15 +0100

    adding src in download_dataset.py

--- download_dataset.py
+++ download_dataset.py
@@ -1,6 +1,9 @@
-from utils.dataset_utils import *
-import os
+import sys
 
-print(os.getcwd())
-url = 'http://host.robots.ox.ac.uk/pascal/VOC/voc2009/VOCtrainval_11-May-2009.tar'
-dataset_dir = get_dataset(url)
+sys.path.append('src')
+
+from utils.dataset_utils import *
+
+if __name__ == '__main__':
+    url = 'http://host.robots.ox.ac.uk/pascal/VOC/voc2009/VOCtrainval_11-May-2009.tar'
+    dataset_dir = get_dataset(url)
